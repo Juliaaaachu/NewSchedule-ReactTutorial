465284c8a09295c309de5488428f4c93bab93e11
Juliaaaachu <juliachu@julias-mbp-5.ads.northwestern.edu>
2022-05-27 23:57:15 -0500

    stylecss

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@
 // export default App;
 
 import React from 'react';
+import './App.css';
 
 const schedule = {
   "title": "CS Courses for 2018-2019",
@@ -53,7 +54,7 @@ const schedule = {
 };
 
 const App = () =>  (
-  <div>
+  <div className="container">
     <Banner title={ schedule.title } />
     <CourseList courses={ schedule.courses } />
   </div>
@@ -64,10 +65,11 @@ const Banner = ({ title }) => (
 );
 
 const CourseList = ({ courses }) => (
-  <div>
-    { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
+  <div className="course-list">
+  { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
   </div>
 );
+
 const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
 
 const getCourseTerm = course => (
@@ -79,8 +81,11 @@ const getCourseNumber = course => (
 );
 
 const Course = ({ course }) => (
-  <div>
-    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+  <div className="card m-1 p-2">
+    <div className="card-body">
+      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+      <div className="card-text">{ course.title }</div>
+    </div>
   </div>
 );
 
